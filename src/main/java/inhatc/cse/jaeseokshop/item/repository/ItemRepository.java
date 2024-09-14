0763632610ff7e62b771efa2a1e13ac0b0f96190
you-fired <15120@naver.com>
2024-09-14 12:16:16 +0900

    3주차 커밋 + 과제

--- src/main/java/inhatc/cse/jaeseokshop/item/repository/ItemRepository.java
+++ src/main/java/inhatc/cse/jaeseokshop/item/repository/ItemRepository.java
@@ -11,7 +11,7 @@ public interface ItemRepository extends JpaRepository<Item,Long> {
     List<Item> findByItemNm(String itemNm);
     List<Item> findByItemNmLike(String itemNm);
     List<Item> findByPriceLessThanOrderByPriceDesc(int price);
-    @Query(value = "select i.itemNm,i.price from Item i where i.itemDetail like %:itemDetail% " +
+    @Query(value = "select i from Item i where i.itemDetail like %:itemDetail% " +
             "order by i.price desc")
     List<Item> findItemDetail(@Param("itemDetail") String itemDetail);
 
@@ -19,5 +19,8 @@ public interface ItemRepository extends JpaRepository<Item,Long> {
             "order by i.price desc",nativeQuery = true)
     List<Item> findItemDetailNative(@Param("itemDetail") String itemDetail);
 
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
 
 }
--- src/test/java/inhatc/cse/jaeseokshop/item/repository/ItemRepositoryTest.java
+++ src/test/java/inhatc/cse/jaeseokshop/item/repository/ItemRepositoryTest.java
@@ -19,12 +19,35 @@ class ItemRepositoryTest {
 
     @Test
     public void findByPriceLessThanOrderByPriceDesc() {
-        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(35000);
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(50000);
         itemList.forEach(System.out::println);
     }
     @Test
     public void findByItemNmLike(){
         List<Item> itemList = itemRepository.findByItemNmLike("%2%");
+        itemList.forEach(System.out::println);
+    }
+
+    @Test
+    public void findByItemNmOrItemDetail(){
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("신상품1","신상품 상세 설명2");
+        itemList.forEach(System.out::println);
+    }
+
+    @Test
+    public void findByPriceLessThan(){
+        List<Item> itemList = itemRepository.findByPriceLessThan(25000);
+        itemList.forEach(System.out::println);
+    }
+
+    @Test
+    public void findByItemDetailTest(){
+        List<Item> itemList = itemRepository.findItemDetail("신상품 상세 설명1");
+        itemList.forEach(System.out::println);
+    }
+    @Test
+    public void findItemDetailNative(){
+        List<Item> itemList = itemRepository.findItemDetail("신상품 상세 설명1");
         itemList.forEach(System.out::println);
     }
 }
