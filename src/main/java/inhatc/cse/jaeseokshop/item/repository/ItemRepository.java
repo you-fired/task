a27d6731a77129e6fe5f47d5917009b958dc4f21
you-fired <15120@naver.com>
2024-10-02 13:27:35 +0900

    6주차 내용 + 과제

--- src/main/java/inhatc/cse/jaeseokshop/item/repository/ItemRepository.java
+++ src/main/java/inhatc/cse/jaeseokshop/item/repository/ItemRepository.java
@@ -2,9 +2,10 @@ package inhatc.cse.jaeseokshop.item.repository;
 import inhatc.cse.jaeseokshop.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
-public interface ItemRepository extends JpaRepository<Item,Long> {
+public interface ItemRepository extends JpaRepository<Item,Long> , QuerydslPredicateExecutor<Item> {
     List<Item> findByItemNm(String itemNm);
     List<Item> findByItemNmLike(String itemNm);
     List<Item> findByPriceLessThanOrderByPriceDesc(int price);
