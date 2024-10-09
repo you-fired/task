818af0e696e7f278e95ae7a1509f0f277aa67ee2
you-fired <15120@naver.com>
2024-10-09 17:49:33 +0900

    10월 9일 오류 잡기 성공

--- src/main/java/inhatc/cse/jaeseokshop/config/SecurityConfig.java
+++ src/main/java/inhatc/cse/jaeseokshop/config/SecurityConfig.java
@@ -4,13 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
+
+
 }
